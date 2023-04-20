834f2ec19668fda12f55589d22f6cdb5b9a6a087
Christian <naresan50@hotmail.com>
2023-04-20 06:46:10 -0500

    calculadora v1

---
+++ ejercicio1/index.js
@@ -0,0 +1,25 @@
+var number1 = document.getElementById('number1').value
+var number2 = document.getElementById('number2').value
+
+const suma = document.getElementById('suma')
+const resta = document.getElementById('resta')
+const multiplicacion = document.getElementById('multiplicacion')
+const divison = document.getElementById('division')
+
+suma.addEventListener('click',function(){
+
+    var resultadosuma= parseInt(number1)+parseInt(number2)
+    alert("el resultado de la operacion es :"+resultadosuma)
+})
+
+resta.addEventListener('click',function(){
+    
+})
+
+multiplicacion.addEventListener('click',function(){
+    
+})
+
+division.addEventListener('click',function(){
+    
+})
